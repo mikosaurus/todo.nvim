6dea987241944017782a89e09115e72fe9117b6c
Anders Johannessen <mikosoft35@gmail.com>
2025-04-19 14:11:45 +0000

    Added filetype in plugin so you dont have to do so per nvim setup

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,8 +1,17 @@
 -- print("hello from init in plugin folder")
 local ft = require'Comment.ft'
+
+vim.filetype.add({
+    extension = {
+        todo = 'todofiles',
+        TODO = 'todofiles',
+    },
+})
+
 ft.set('todofiles', '#%s')
 ft.set('todo', '#%s')
 ft.set('TODO', '#%s')
 
 vim.cmd([[hi @done guifg=#5CE65C]])
 vim.cmd([[hi @cancelled guifg=#FF7081]])
+
